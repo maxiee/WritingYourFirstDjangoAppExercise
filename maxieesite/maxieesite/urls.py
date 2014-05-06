de2f78e472060ff4ff63627de8423aee0ea527f1
Maxiee <maxieewong@gmail.com>
2014-05-06 12:23:18 +0800

    add simple views

--- maxieesite/maxieesite/urls.py
+++ maxieesite/maxieesite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from maxieesite import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,4 +15,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^polls/$', views.index, name = 'index'),
+
+    url(r'^polls/(?P<poll_id>\d+)/$', views.detail, name = 'detail'),
+
+    url(r'^polls/(?P<poll_id>\d+)/results/$', views.results, name='results'),
+
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
