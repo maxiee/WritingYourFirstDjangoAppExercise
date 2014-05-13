bf0c47ddf05e61837adc7f329338596919828959
Maxiee <maxieewong@gmail.com>
2014-05-13 22:55:56 +0800

    add hours ahead view

--- maxieesite/maxieesite/urls.py
+++ maxieesite/maxieesite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from helloworld.views import hello, current_datetime
+from helloworld.views import hello, current_datetime, hours_ahead
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$',hello),
-    url(r'^time/$', current_datetime)
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$',hours_ahead),
 )
+
 #^: require that the pattern matches the start of the string.
 #$: require that the pattern matches the end of the string.
