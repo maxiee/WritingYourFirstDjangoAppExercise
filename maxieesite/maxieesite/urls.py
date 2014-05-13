1037e491682f3be2fee499260799d93e907e7f2d
Maxiee <maxieewong@gmail.com>
2014-05-13 22:14:22 +0800

    add current_datetime view

--- maxieesite/maxieesite/urls.py
+++ maxieesite/maxieesite/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from helloworld.views import hello, current_datetime
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,7 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^hello/$','helloworld.views.hello'),
+    url(r'^hello/$',hello),
+    url(r'^time/$', current_datetime)
 )
 #^: require that the pattern matches the start of the string.
 #$: require that the pattern matches the end of the string.
