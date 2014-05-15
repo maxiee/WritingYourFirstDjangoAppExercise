a08622d1bc72222e98741099a40220e42762f759
Maxiee <maxieewong@gmail.com>
2014-05-15 13:47:16 +0800

    template processing complex object

--- scripts/templateTest.py
+++ scripts/templateTest.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django import template
+import datetime
 
 settings.configure()
 t = template.Template('My name is {{ name }}.')
@@ -7,3 +8,13 @@ c = template.Context({'name': 'Judy'})
 print t.render(c)
 c = template.Context({'name': "Maxiee"})
 print t.render(c)
+
+person = {'name': 'Maxiee', 'age': '24'}
+t2 = template.Template('{{person.name}} is {{person.age}} years old.')
+c2 = template.Context({'person': person})
+print t2.render(c2)
+
+d = datetime.date(1999,9,9)
+t3 = template.Template('The month is {{date.month}} and the year is {{date.year}}.')
+c3 = template.Context({'date':d})
+print t3.render(c3)
